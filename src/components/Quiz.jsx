2b0d75f047c81c868023467a96d3e84837f649e2
kxczmxr <jakubkaczmarski4@gmail.com>
2025-01-09 00:41:58 +0100

    Add quiz completion screen and shuffle answers
    A completion screen with a message and image is now displayed when the user finishes the quiz. Answers are also shuffled to add variety to the quiz-taking experience.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,22 +1,36 @@
 import {useState} from "react";
 import QUESTIONS from "../questions";
+import completeImg from "../assets/quiz-complete.png";
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+    const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
     function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswer)=>{
             return [...prevUserAnswer, selectedAnswer];
         });
     }
+    if(quizIsComplete) {
+        return (
+            <div id={'summary'}>
+                <img src={completeImg} alt="complete"/>
+                    <h2>Quiz is complete!</h2>
+            </div>
+        )
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
+
 
     return (
         <div id={'quiz'}>
             <div id={'question'}>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id={'answers'}>
-                    {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    {shuffledAnswers.map((answer) => (
                         <li key={answer} className={'answer'}>
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>
